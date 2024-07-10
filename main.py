8dd854194180ce570a3bd9db762c0b05ea217528
Binaykumar12 <binaykumarkurmi78@gmail.com>
2024-07-10 14:22:22 +0545

    .

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-
 import requests
 from bs4 import BeautifulSoup
 import time
 import random
 
 # URL of the page to scrape
-url = "https://www.geeksforgeeks.org/what-is-web-scraping-and-how-to-use-it/"
+url = "https://quotes.toscrape.com/"
 
 # Make an HTTP GET request to the URL
 response = requests.get(url)
 
 # Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(response.text, "html.parser")
 
-# Extract the main content of the article
-article_content = soup.find("div", class_="entry-content")
+# Extract and print quotes
+quotes = soup.find_all("span", class_="text")
+for quote in quotes:
+    print(quote.text)
 
-# Extract and print the text content of the article
-paragraphs = article_content.find_all("p")
-for para in paragraphs:
-    print(para.get_text())
+# Extract and print authors
+authors = soup.find_all("small", class_="author")
+for author in authors:
+    print(author.text)
 
 # Sleep for a random interval between 1 and 5 seconds
 sleep_time = random.uniform(1, 5)
